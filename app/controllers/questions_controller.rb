3dba695cf0cac01c4f45398a615564fa7ee7f056
aleks vangjelofski <aleks.vangjelofski@gmail.com>
2023-02-20 12:59:50 +0100

    Stupid coach on rails

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,4 +1,18 @@
 class QuestionsController < ApplicationController
+
   def ask
+  end
+
+  def answer
+    # If the message is I am going to work, the coach will answer Great!
+    if params[:question] == "I am going to work"
+      @answer = "Great!"
+    # If the message has a question mark ? at the end, the coach will answer Silly question, get dressed and go to work!.
+    elsif params[:question] == params[:question].end_with?("?")
+      @answer = "Silly question, get dressed and go to work!"
+    # Otherwise the coach will answer I don't care, get dressed and go to work!
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'ask', to: "questions#ask"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  get 'ask', to: 'questions#ask'
+  get 'answer', to: 'questions#answer'
   # Defines the root path route ("/")
   # root "articles#index"
 end
